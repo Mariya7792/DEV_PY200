867074f6012198a22c446e5c3dd317cf3408df18
marbur <sophi5@yandex.ru>
2024-01-24 21:11:26 +0300

    Практические задания по 3 теме

--- Инкапсуляция, наследование, полиморфизм/Практические задания/task1_Figure_super/main.py
+++ Инкапсуляция, наследование, полиморфизм/Практические задания/task1_Figure_super/main.py
@@ -8,10 +8,9 @@ class Figure:
 
 class Rectangle(Figure):
     def __init__(self, a, b, name=None):
-        # TODO вызвать конструктор базового класса с помощью super
+        super().__init__(name)
         self.a = a
         self.b = b
-
 
 if __name__ == "__main__":
     rect = Rectangle(5, 10, 'rect_fig')
