d2b517b4d2fce82ac5399262bdc46dd6ec11531a
marbur <sophi5@yandex.ru>
2024-01-24 20:55:44 +0300

    Лабораторная 2

---
+++ Атрибуты и методы/Лабораторная 2/Класс Library/test.py
@@ -0,0 +1,14 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+a = BOOKS_DATABASE[-1]['id'] + 1
+print(a)
